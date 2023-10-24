ee88ebc68124de8b8fd8e0a48daf254877ff71b3
zu-zi <735055854@qq.com>
2023-10-24 18:46:49 +0800

    test#5

---
+++ #5-文雯/task2/comment.js
@@ -0,0 +1,45 @@
+const arraycomment = [
+  {
+    username: `bili_55590762952`,
+    remark: `想做壁纸有机会吗`,
+    time: `2022-10-07 14:24`,
+    tx: `2`
+  },
+  {
+    username: `晚安不要钱但有爱`,
+    remark: `祖国的万里山河，怎会不美`,
+    time: `2022-10-06 22:54`,
+    tx: 3
+  },
+  {
+    username: `骑笨象的大企鹅`,
+    remark: `请问有度盘版吗？想要一个高清的做壁纸`,
+    time: `2022-10-07 12:51`,
+    tx: 4
+  },
+  {
+    username: `str1pe`,
+    remark: `求调色教程`,
+    time: `2022-10-06 23:17`,
+    tx: 5
+  },
+  {
+    username: `_噜啦啦_噜啦啦_`,
+    remark: `想去北京`,
+    time: `2022-10-06 22:50`,
+    tx: 6
+  },
+  {
+    username: `Ceylannna`,
+    remark: `要不要这么绝啊`,
+    time: `2022-10-07 08:38`,
+    tx: 7
+  },
+  {
+    username: `四月客`,
+    remark: `在旁边执勤  
+可惜没有up发现美的眼睛[大哭]`,
+    time: `2022-10-07 12:20`,
+    tx: 8
+  }
+]
